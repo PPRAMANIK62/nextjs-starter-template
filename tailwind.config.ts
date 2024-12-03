dae51456fcdb399ca43689d8f41bd5222b77d762
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-12-03 15:45:00 +0530

    modified tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,14 @@
 import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
