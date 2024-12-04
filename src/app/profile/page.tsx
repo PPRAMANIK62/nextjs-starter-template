f43701bbcf8cd0f32cfdecdf94f9c58670bb2abd
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-12-04 11:37:33 +0530

    profile page user card done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardBody, User } from "@nextui-org/react";
 import { getServerSession } from "next-auth";
 
 import { options } from "@/config/auth";
@@ -5,7 +6,20 @@ import { options } from "@/config/auth";
 const ProfilePage = async () => {
   const session = await getServerSession(options);
 
-  return <pre>{JSON.stringify(session)}</pre>;
+  return (
+    <Card className="max-w-md mx-auto mt-4">
+      <CardBody>
+        <User
+          name={session?.user?.name}
+          description={session?.user?.email}
+          avatarProps={{
+            showFallback: !session?.user?.image,
+            src: session?.user?.image || "",
+          }}
+        />
+      </CardBody>
+    </Card>
+  );
 };
 
 export default ProfilePage;
